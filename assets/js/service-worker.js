830ac7aacaff72d0096d5ea042ad873e83b3b62e
c0r3xof <c0r3xof@gmail.com>
2024-11-07 11:24:06 -0300

    Update service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,21 +1,38 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('temporizador-cache').then(function(cache) {
       return cache.addAll([
+        '/',
         '/index.html',
         '/assets/css/style.css',
         '/assets/js/script.js',
+        '/assets/json/manifest.json',
         '/assets/imgs/icon-192x192.png',
-        '/assets/imgs/icon-512x512.png',
+        '/assets/imgs/icon-512x512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['temporizador-cache'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
